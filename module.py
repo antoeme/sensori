b7c18738b08493f23bd4d08032f2fc070e962faf
danycer97 <109142989+danycer97@users.noreply.github.com>
2022-07-15 11:11:37 +0200

    creato modulo parsing html temperature

---
+++ module.py
@@ -0,0 +1,25 @@
+#modulo per il parsing html delle temperature
+
+from cgitb import html, text
+from unittest import result
+from bs4 import BeautifulSoup
+import requests
+
+
+num_sensori = 4 
+#url = "StatusTemperature.html"
+
+#result = requests.get(url)
+#print(result.text)
+
+with open("StatusTemperature.html","r") as f:
+    doc = BeautifulSoup(f,"html.parser")
+
+#print(doc.prettify)
+
+tags = doc.find_all(class_ = "temp" )
+temps= []
+for l in range(num_sensori):
+    t = tags[l].string
+    temps.append(t)
+print(temps)
