86561aa64b47881dd3dc4b65d7bb69b2001a8c0d
danycer97 <109142989+danycer97@users.noreply.github.com>
2022-08-10 13:05:32 +0200

    aggiunta basic auth

--- app.py
+++ app.py
@@ -3,19 +3,38 @@ import json
 from flask import Flask,jsonify
 from requests import request
 import module_temp as mt
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "daniele": generate_password_hash("Cisco123"),
+    "antonio": generate_password_hash("Dtlab123")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+
+
 
 @app.route('/prova')
+@auth.login_required
 def helloworld():
     return jsonify({"about": " Helloworld !"})
 
 @app.route('/temp', methods=['GET'])   #rotta per la get delle temperature
+@auth.login_required
 def get_temp(): 
     json_temps = json.dumps(mt.get_temps())   # dump della lista delle temperature presa in module_temp.py
     return json_temps
 
 @app.route('/temp/<int:id_sensore>', methods=['GET', 'POST'])
+@auth.login_required
 def get_temp_id(id_sensore):
     if((id_sensore>4) or (id_sensore<1)):
         return ("Errore: ID sensore non presente!")
